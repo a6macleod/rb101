0d1c10f7e8c613eab414769d2af4db461d318d9c
Andrew MacLeod <A6macleod@gmail.com>
2022-07-16 15:25:02 -0400

    Lesson2 Rock, Paper, Scissors project

---
+++ lesson2/rock_paper_scissors.rb
@@ -0,0 +1,114 @@
+READABLE_CHOICES = ["rock (r)", "paper (p)", "scissors (sc)", "lizard (l)", "spock (sp)"]
+VALID_CHOICES = {
+  r: "rock",
+  p: "paper",
+  sc: "scissors",
+  l: "lizard",
+  sp: "spock",
+  rock: "rock",
+  paper: "paper",
+  scissors: "scissors",
+  lizard: "lizard",
+  spock: "spock"
+}
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "rock" && second == "lizard") ||
+    (first == "scissors" && second == "paper") ||
+    (first == "scissors" && second == "lizard") ||
+    (first == "paper" && second == "rock") ||
+    (first == "paper" && second == "spock") ||
+    (first == "lizard" && second == "paper") ||
+    (first == "lizard" && second == "spock") ||
+    (first == "spock" && second == "rock") ||
+    (first == "spock" && second == "scissors")
+end
+
+def increment_score(winner, p_score, c_score)
+  if winner == "player"
+    p_score << "1"
+  elsif winner == "computer"
+    c_score << "1"
+  else
+    return
+  end
+end
+
+def round_results(player, computer)
+  if win?(player, computer)
+   "player"
+  elsif win?(computer, player)
+   "computer"
+  else
+    "tie"
+  end
+end
+
+def display_winner(winner)
+  if winner == "player" || winner == "computer"
+    prompt("#{winner} wins!")
+  else
+    prompt("Its a tie!")
+  end
+end
+
+def display_scores(p_score, c_score)
+  prompt("Player Score: #{p_score.length}")
+  prompt("Computer Score: #{c_score.length}")
+end
+
+def end_game?(p_score, c_score)
+  if p_score.length == 3 || c_score.length == 3
+    true
+  end
+end
+
+def display_grand_winner(round_winner)
+  prompt("#{round_winner} is the final winner! Good game!")
+end
+
+player_score = ""
+computer_score = ""
+
+prompt("Rock Paper Scissors! best of 3 wins!")
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{READABLE_CHOICES.join(', ')}")
+    choice = gets.chomp.downcase
+
+    if VALID_CHOICES.include?(choice.to_sym)
+      break
+    else
+      puts "That is not a valid choice"
+    end
+  end
+
+  computer_choice = VALID_CHOICES.to_a.sample
+
+  prompt("You chose: #{VALID_CHOICES[choice.to_sym]}; Computer chose: #{computer_choice[1]}")
+
+  round_winner = round_results(VALID_CHOICES[choice.to_sym], computer_choice[1])
+
+  increment_score(round_winner, player_score, computer_score)
+
+  display_winner(round_winner)
+
+  display_scores(player_score, computer_score)
+
+  if end_game?(player_score, computer_score)
+    display_grand_winner(round_winner)
+    break
+  end
+
+  prompt("Play again? (y to play again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt("Thanks for playing. Good bye!")
